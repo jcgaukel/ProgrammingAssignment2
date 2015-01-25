e0a09cfcdd69c58628e989d04e3a6f81ac73d5c9
J.C. Gaukel <jc.github@gaukel.org>
2015-01-24 19:01:30 -0600

    First Draft of Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,84 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# Author1:      J.C. Gaukel (jc.github@gaukel.org)
+# Date:         January 23rd, 2015
+# Course:       R Programming
+# 
+# Function:     makeCacheMatrix 
+# 
+# Description:  
+#              
+# 
+# Parameters :  x- a square matrix 
+# 
+# Return:       inverse of x
+# 
+# Examples of Usage: 
+# 
+#    
+#    
+#   
+#   
+# 
+# 
 
+makeCacheMatrix <- function(x = matrix()) {
+  invMatrix <- NULL
+
+  set <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+
+  get <- function() x
+  
+  setinverse <- function(inverseMatrix) invMatrix <<- inverse 
+  
+  getinverse <- function() invMatrix
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse=getinverse)
+    
 }
 
 
 ## Write a short comment describing this function
 
+# Author1: J.C. Gaukel (jc.github@gaukel.org)
+# Date:    January 23rd, 2015
+# Course:  R Programming
+# 
+# Function   : cacheSolve 
+# 
+# Description: 
+#              
+# 
+# Parameters : x- a square matrix 
+# 
+# Return     : inverse of x
+# 
+# Examples of Usage: 
+# 
+#    
+#    
+#   
+#   
+# 
+# 
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+  invMatrix <- x$getinverse()
+  
+  if(!is.null(invMatrix)) {
+    message("getting cached data")
+    return(invMatrix)
+  }
+  
+  data <- x$get()
+  invMatrix <- solve(data, ...)
+  x$setinverse(invMatrix)
+  invMatrix
+  
 }
